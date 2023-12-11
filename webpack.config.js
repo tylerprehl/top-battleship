8c5c5fc84407abd120d6288647a2c6a9a22a6719
Tyler Prehl <tyler@prehl.us>
2023-12-10 20:13:23 -0500

    Install html webpack plugin and clean up webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -13,25 +13,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  /*
   plugins: [
     new HtmlWebpackPlugin({
       title: 'JavaScript Practice',
       template: './index.html',
     }),
   ],
-  */
   module: {
     rules: [
       {
         test: /\.(css)$/,
         use: [
-          // Adds CSS to the DOM by injecting a `<style>` tag
           'style-loader',
-          // Interprets `@import` and `url()` like `import/require()` and will resolve them
           'css-loader',
         ],
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
     ],
   },
@@ -41,6 +37,6 @@ module.exports = {
     },
   },
   watchOptions: {
-    ignored: /node_modules/
-  }
+    ignored: /node_modules/,
+  },
 };
