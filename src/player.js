2275a363b1b3e73a9ba3b78ac470b929555d9bdf
Tyler Prehl <tyler@prehl.us>
2023-12-14 20:55:34 -0500

    Update Player with winCount and playerBoard
    Implement increaseWinCount, getWinCount, and include playerBoard as a
    parameter for creating a player.

--- src/player.js
+++ src/player.js
@@ -1,5 +1,15 @@
-function createPlayer(name) {
-  return {name};
+function createPlayer(playerName, playerBoard) { 
+  let winCount = 0;
+
+  function getWinCount() {
+    return winCount;
+  }
+
+  function increaseWinCount() {
+    winCount++;
+  }
+
+  return { playerName, playerBoard, getWinCount, increaseWinCount };
 }
 
 export { createPlayer };
--- src/player.test.js
+++ src/player.test.js
@@ -1,6 +1,18 @@
 import * as Player from './player';
+import * as Gameboard from './gameboard';
 
-it('Test Player Creation', () => {
+it('Test playerName', () => {
   const myPlayer = Player.createPlayer('Tyler');
-  expect(myPlayer.name).toBe('Tyler');
+  expect(myPlayer.playerName).toBe('Tyler');
+});
+
+it('Test playerBoard', () => {
+  const myPlayer = Player.createPlayer('Tyler', Gameboard.createGameboard());
+  expect(myPlayer.playerBoard.gameboard[0][0].ship).toBe('-');
+});
+
+it('Test increase/get WinCount', () => {
+  const myPlayer = Player.createPlayer('Tyler', Gameboard.createGameboard());
+  myPlayer.increaseWinCount();
+  expect(myPlayer.getWinCount()).toBe(1);
 });
