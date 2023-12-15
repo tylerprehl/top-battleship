79fd9f9ff5a5c2c429400c1681806ae8a8282c09
Tyler Prehl <tyler@prehl.us>
2023-12-14 20:57:42 -0500

    Draft start of gameplay
    Create a rough draft of the flow of gameplay - player setup, and player1
    gameboard setup.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # The Odin Project - Battleship
 This project's goals are tailored towards practicing Test Driven Development with JS and Jest.
+
+### To Do
+- TBD
+
+### Main Takeaways
+- 
+
+### Other Interesting Things Learned
+- the Array.prototype.forEach() loop cannot be broken
+
+### Notes for battleship.js Structure
+- 'global' vars: the players
+
+### Notes for Gameplay Design/Order
+- create player
+- build player board
+- 'pass computer' screen
+- create player/AI
+- build player/AI board
+- game starts...
--- src/battleship.js
+++ src/battleship.js
@@ -1,10 +1,54 @@
-import * as Ship from "./ship.js";
-import * as Player from "./player.js";
+import * as Player from './player.js';
+import * as Gameboard from './gameboard.js';
+import * as Ship from './ship.js';
 
-console.log("Testing");
+console.log('Starting Game');
 
-const tylerShip = Ship.createShip(3);
-console.log(tylerShip);
+console.log('Get Player 1 Name');
+const player1Name = prompt('Player 1 Name');
+const player1 = Player.createPlayer(player1Name, Gameboard.createGameboard());
 
-const tylerPlayer = Player.createPlayer('Tyler');
-console.log(tylerPlayer);
+console.log('Get Player 2 Name');
+const player2Name = prompt('Player 2 Name');
+const player2 = Player.createPlayer(player2Name, Gameboard.createGameboard());
+
+const shipLengths = [2,2,3,3,4]
+
+console.log('Get Player 1 Ship Locations');
+let shipsPlacedCount = 0; 
+while(shipsPlacedCount < 5) {
+  try {
+    alert(`The first ship will be ${shipLengths[shipsPlacedCount]} in length`);
+
+    // getShipOrientation will be a radio button selection eventually
+    let orientation = prompt('Enter orientation: "v" for "vertical", ' + 
+      'or "h" for "horizontal"');
+    if (orientation === 'v') {
+      orientation = 'vertical';
+    }
+    else if (orientation === 'h') {
+      orientation = 'horizontal';
+    }
+    else {
+      throw new Error('invalid orientation');
+    }
+
+    // getShipStartCoordinates will be some type of table-click selection
+    // (would be cool if there's a faded version of ship until placed)
+    const coordinates = prompt('Enter coordinates in the format' + 
+    '"rowIndex,colIndex".\nEx: 1,2 would place the start of the ship ' +
+    'in the 2nd row at the 3rd column');
+    const individualCoordinates = coordinates.split(',');
+    player1.playerBoard.placeShip(
+      Ship.createShip(shipLengths[shipsPlacedCount]),
+      orientation,
+      individualCoordinates[0],
+      individualCoordinates[1]
+    )
+    shipsPlacedCount++;
+  } catch (e) {
+    console.alert(`Error: ${e.message}\nPlease try again`);
+  }
+}
+
+console.log('Get Player 2 Ship Locations');
